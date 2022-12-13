7e840e6464eb78b7b3399b0b9106ca73cc51e119
LL <limalucas454@gmail.com>
2022-12-13 18:23:50 -0300

    makin a more final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,82 @@
-import { useState, useEffect } from "react";
-import DiceBox from "@3d-dice/dice-box";
+import { useState } from "react";
+import { Dice } from "./components/diceBox";
 import DiceTray from "./components/DiceTray";
 
 function App() {
-  let DiceBoxInstance;
-
-  useEffect(() => {
-    DiceBoxInstance = new DiceBox(
-      "#dice-box", // target DOM element to inject the canvas for rendering
-      {
-        id: "dice-canvas", // canvas element id
-        assetPath: "/assets/dice-box/",
-        startingHeight: 8,
-        throwForce: 6,
-        spinForce: 5,
-        lightIntensity: 0.9,
-        scale: 5,
+  Dice.init().then(() => {
+    // clear dice on click anywhere on the screen
+    document.addEventListener("mousedown", () => {
+      const diceBoxCanvas = document.getElementById("dice-canvas");
+      if (window.getComputedStyle(diceBoxCanvas).display !== "none") {
+        Dice.hide().clear();
       }
-    );
-    DiceBoxInstance.init().then(() => {
-      // clear dice on click anywhere on the screen
-      document.addEventListener("mousedown", () => {
-        const diceBoxCanvas = document.getElementById("dice-canvas");
-        if (window.getComputedStyle(diceBoxCanvas).display !== "none") {
-          DiceBoxInstance.hide().clear();
-        }
-      });
     });
-  }, []);
+  });
 
   const rollDice = (diceValues) => {
-    // trigger the dice roll
-    debugger;
-    DiceBoxInstance.show().roll(["1d4"]);
+    //for each dice value in the array, roll the dice if the value is bigger than 0
+    diceValues.forEach((diceValue) => {
+      if (diceValue[0] > 0) {
+        Dice.show().roll(diceValue);
+      }
+    });
   };
 
-  const [diceValues, setDiceValues] = useState(["1d4"]);
+  const [diceValues, setDiceValues] = useState([
+    "0d4",
+    "0d6",
+    "0d8",
+    "0d10",
+    "0d12",
+    "0d20",
+  ]);
 
   return (
     <div className="grid items-center justify-center h-screen">
-      <div className="grid gap-10">
-        <h1 className="text-6xl">Rolagem de Dados</h1>
+      <div className="grid  gap-10">
+        <h1 className="text-6xl text-center">Rolagem de Dados</h1>
         <button
           className="mx-auto border rounded py-2 px-4 bg-orange-600 w-48 text-white"
           onClick={() => rollDice(diceValues)}
         >
           Rolar dados
         </button>
-        <DiceTray diceValues={diceValues} setDiceValues={setDiceValues} />
+        <div className="grid gap-4">
+          <div className="grid gap-2 grid-flow-col">
+            <DiceTray
+              diceTitle="d4"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+            <DiceTray
+              diceTitle="d6"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+            <DiceTray
+              diceTitle="d8"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+          </div>
+          <div className="grid gap-2 grid-flow-col">
+            <DiceTray
+              diceTitle="d10"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+            <DiceTray
+              diceTitle="d12"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+            <DiceTray
+              diceTitle="d20"
+              diceValues={diceValues}
+              setDiceValues={setDiceValues}
+            />
+          </div>
+        </div>
       </div>
       <div id="dice-box"></div>
     </div>
--- src/components/DiceTray.jsx
+++ src/components/DiceTray.jsx
@@ -1,18 +1,67 @@
 import { useState, useEffect } from "react";
 import { BiPyramid } from "react-icons/bi";
+import { FaDiceD6, FaDiceD20 } from "react-icons/fa";
+import { GiDiceEightFacesEight, GiD10, GiD12 } from "react-icons/gi";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
-const DiceTray = ({ diceValues, setDiceValues }) => {
-  const [diceCount, setDiceCount] = useState(1);
+const DiceTray = ({ diceValues, setDiceValues, diceTitle }) => {
+  const [diceCount, setDiceCount] = useState(0);
+
+  const diceIcons = {
+    d4: <BiPyramid className="large-icon" />,
+    d6: <FaDiceD6 className="large-icon" />,
+    d8: <GiDiceEightFacesEight className="large-icon" />,
+    d10: <GiD10 className="large-icon" />,
+    d12: <GiD12 className="large-icon" />,
+    d20: <FaDiceD20 className="large-icon" />,
+  };
 
   useEffect(() => {
-    //change the first element of the diceValue array to diceCount+"d4"
-    setDiceValues([diceCount + "d4", ...diceValues.slice(1)]);
+    //if the diceTitle is d4 change the first element of the array to be diceValue + d4
+    if (diceTitle === "d4") {
+      setDiceValues([diceCount + diceTitle, ...diceValues.slice(1)]);
+    }
+    //if the diceTitle is d6 change the second element of the array to be diceValue + d6
+    if (diceTitle === "d6") {
+      setDiceValues([
+        ...diceValues.slice(0, 1),
+        diceCount + diceTitle,
+        ...diceValues.slice(2),
+      ]);
+    }
+    //if the diceTitle is d8 change the third element of the array to be diceValue + d8
+    if (diceTitle === "d8") {
+      setDiceValues([
+        ...diceValues.slice(0, 2),
+        diceCount + diceTitle,
+        ...diceValues.slice(3),
+      ]);
+    }
+    //if the diceTitle is d10 change the fourth element of the array to be diceValue + d10
+    if (diceTitle === "d10") {
+      setDiceValues([
+        ...diceValues.slice(0, 3),
+        diceCount + diceTitle,
+        ...diceValues.slice(4),
+      ]);
+    }
+    //if the diceTitle is d12 change the fifth element of the array to be diceValue + d12
+    if (diceTitle === "d12") {
+      setDiceValues([
+        ...diceValues.slice(0, 4),
+        diceCount + diceTitle,
+        ...diceValues.slice(5),
+      ]);
+    }
+    //if the diceTitle is d20 change the sixth element of the array to be diceValue + d20
+    if (diceTitle === "d20") {
+      setDiceValues([...diceValues.slice(0, 5), diceCount + diceTitle]);
+    }
   }, [diceCount]);
 
   return (
     <div className="grid justify-around grid-flow-col w-56 border-orange-600 gap-2 border-2 px-2 py-6 rounded">
-      <BiPyramid className="large-icon" />
+      {diceIcons[diceTitle]}
       <div className="grid grid-flow-col gap-2 w-24 justify-evenly">
         <button onClick={() => setDiceCount(diceCount + 1)}>
           <AiOutlinePlusCircle className="icon" />
